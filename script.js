11ac0c3eebe993c3c3fa51f4967ead4df52ea140
Felipe Gabriel Gomes <143435358+Felipe-GGomes@users.noreply.github.com>
2025-10-26 12:21:16 -0300

    arrumando o codigo, arrumando js, comentando o codigo, nada alterado realmente apenas melhorei o codigo e comentei

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 <script>
 const foto = document.querySelector('.foto-quemsou');
 foto.addEventListener('click', function() {
-  foto.classList.toggle('ampliada');
+  foto.classList.toggle('ampliada')
 });
 </script>
